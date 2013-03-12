65937be4dd7fd8239d7b11cf9e4d8a0a83adc728
Einar Baumann <einar.baumann@gmail.com>
2013-03-12 11:00:25 +0100

    Lagt til felter og loginmetode

---
+++ src/no/ntnu/gruppe47/db/entities/Alert.java
@@ -0,0 +1,10 @@
+package no.ntnu.gruppe47.db.entities;
+
+/**
+ * Created by:
+ * User: Einar
+ * Date: 12.03.13
+ * Time: 10:47
+ */
+public class Alert {
+}
--- src/no/ntnu/gruppe47/db/entities/User.java
+++ src/no/ntnu/gruppe47/db/entities/User.java
@@ -1,11 +1,38 @@
 package no.ntnu.gruppe47.db.entities;
 
-public class User {
-	
-	private String name;
+import java.util.ArrayList;
 
-	public User(String name) {
-		this.name = name;
-	}
+public class User {
+
+    private String name;
+    private String username;
+    private String pasword;
+    private int userId;
+    private String email;
+    private int phone;
+    private ArrayList<Alarm> alarmList = new ArrayList<Alarm>();
+    private ArrayList<Calendar> calendarList = new ArrayList<Calendar>();
+    private ArrayList<Alert> alertList = new ArrayList<Alert>();
+
+    public User(String name, String username, String pasword, int userId,
+                String email, int phone, Calendar personalCalendar) {
+        this.name = name;
+        this.username = username;
+        this.pasword = pasword;
+        this.userId = userId;
+        this.email = email;
+        this.phone = phone;
+        calendarList.add(personalCalendar);
+    }
+
+    public boolean login(String username, String password) {
+        if (!(username.equals(this.username) && password.equals(this.pasword))) {
+            System.out.println("Username or password incorrect.");
+            return false;
+        }
+        // TODO: Prøv å koble til databasen. Hvis det fungerer, returner true.
+        System.out.println("Unable to connect to database.");
+        return false; // Returnerer false hvis man ikke får koblet til databasen.
+    }
 
 }
