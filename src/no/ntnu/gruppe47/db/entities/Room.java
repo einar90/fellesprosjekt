4c4df281828813b5dc5a368f6bf09c7a66701b54
Sindre Nistad <sindre_123@msn.com>
2013-03-14 12:27:20 +0100

    Ordnet merge-konflikt: room_id er nå ikke i konstruktorene

--- src/no/ntnu/gruppe47/db/entities/Room.java
+++ src/no/ntnu/gruppe47/db/entities/Room.java
@@ -5,43 +5,6 @@ import no.ntnu.gruppe47.db.RoomSQL;
 
 public class Room {
 
-<<<<<<< HEAD
-    private int room_id;
-    private String roomNumber;
-    private int capacity;
-
-    public Room(String roomNumber, int capacity) {
-        this(roomNumber);
-        this.capacity = capacity;
-    }
-
-    public Room(String roomNumber) {
-        this.roomNumber = roomNumber;
-        // TODO: Sync with database
-    }
-
-    public int getRoom_id() {
-        return room_id;
-    }
-
-    public void setRoomNumber(String roomNumber) {
-        if (roomNumber == null)
-            throw new IllegalArgumentException("Cannot delete a room number");
-        this.roomNumber = roomNumber;
-    }
-
-    public String getRoomNumber() {
-        return roomNumber;
-    }
-
-    public void setCapacity(int capacity) {
-        this.capacity = capacity;
-    }
-
-    public int getCapacity() {
-        return capacity;
-    }
-=======
 private int room_id;
 private String roomNumber;
 private int capacity;
@@ -86,6 +49,5 @@ public void setCapacity(int capacity){
 public int getCapacity(){
 	return capacity;
 }
->>>>>>> Lagt til RoomSQL-klassen, samt synkronisert Room-klassen med databasen
 
 }
