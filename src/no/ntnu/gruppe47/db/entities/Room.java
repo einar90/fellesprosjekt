5da44e157ed3d0e827098528b625212cc671478f
Einar Baumann <einar.baumann@gmail.com>
2013-03-14 12:18:02 +0100

    Fjernet room_id fra Room konstrukøtren

--- src/no/ntnu/gruppe47/db/entities/Room.java
+++ src/no/ntnu/gruppe47/db/entities/Room.java
@@ -2,41 +2,40 @@ package no.ntnu.gruppe47.db.entities;
 
 public class Room {
 
-private int room_id;
-private String roomNumber;
-private int capacity;
+    private int room_id;
+    private String roomNumber;
+    private int capacity;
 
-public Room(String roomNumber, int capacity){
-	this(roomNumber);
-	this.capacity = capacity;
-}
+    public Room(String roomNumber, int capacity) {
+        this(roomNumber);
+        this.capacity = capacity;
+    }
 
-public Room(String roomNumber){
-	this.room_id; 
-	this.roomNumber = roomNumber;
-	// TODO: Sync with database
-}
+    public Room(String roomNumber) {
+        this.roomNumber = roomNumber;
+        // TODO: Sync with database
+    }
 
-public int getRoom_id(){
-	return room_id;
-}
+    public int getRoom_id() {
+        return room_id;
+    }
 
-public void setRoomNumber(String roomNumber){
-	if (roomNumber == null)
-		throw new IllegalArgumentException("Cannot delete a room number");
-	this.roomNumber = roomNumber;
-}
+    public void setRoomNumber(String roomNumber) {
+        if (roomNumber == null)
+            throw new IllegalArgumentException("Cannot delete a room number");
+        this.roomNumber = roomNumber;
+    }
 
-public String getRoomNumber(){
-	return roomNumber;
-}
+    public String getRoomNumber() {
+        return roomNumber;
+    }
 
-public void setCapacity(int capacity){
-	this.capacity = capacity;
-}
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
 
-public int getCapacity(){
-	return capacity;
-}
+    public int getCapacity() {
+        return capacity;
+    }
 
 }
